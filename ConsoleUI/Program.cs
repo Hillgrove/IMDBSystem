87fb6a1e8e771482037de4e81774b1bea67cacd5
Jesper Højlund <github@hillgrove.dk>
2025-04-02 02:42:02 +0200

    chore: removed and changed DB password.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,9 +9,6 @@ var config = new ConfigurationBuilder()
 string connectionString = config.GetConnectionString("Default")
     ?? throw new InvalidOperationException("Connection string 'Default' not found in user secrets.");
 
-//string connectionString = "Data Source=mssql11.unoeuro.com;Initial Catalog=hillgrove_dk_db_datamatiker;User ID=hillgrove_dk;Password=9coz8Awpel0Jo5dEvA9jXQiNRQseko2quzus;Encrypt=True;TrustServerCertificate=True;";
-
-
 
 ITitleRepository titleRepository = TitleRepositoryList.Instance;
 //ITitleRepository repository = new TitleRepositorySql("YourConnectionString");
