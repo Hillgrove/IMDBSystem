79063f16da29a0b770f55e440432e5c924f4784e
Jesper Højlund <github@hillgrove.dk>
2025-04-04 15:49:14 +0200

    feat: Add manual SQL connection string input option

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,10 +3,29 @@ using Data;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 
-var vaultUri = new Uri("https://hillvault.vault.azure.net/");
-var client = new SecretClient(vaultUri, new DefaultAzureCredential());
-KeyVaultSecret secret = client.GetSecret("Obl-SQL-ConnectionString");
-string connectionString = secret.Value;
+string serverName = "";
+string database = "hillgrove_dk_db_datamatiker";
+string userId = "";     
+string password = "";
+
+string connectionString;
+
+if (!string.IsNullOrWhiteSpace(serverName) &&
+    !string.IsNullOrWhiteSpace(userId) &&
+    !string.IsNullOrWhiteSpace(password))
+{
+    // Use manually filled values
+    connectionString =
+        $"Server={serverName};Database={database};User ID={userId};Password={password};Encrypt=True;TrustServerCertificate=True;";
+}
+else
+{
+    // Use Azure Key Vault
+    var vaultUri = new Uri("https://hillvault.vault.azure.net/");
+    var client = new SecretClient(vaultUri, new DefaultAzureCredential());
+    KeyVaultSecret secret = client.GetSecret("Obl-SQL-ConnectionString");
+    connectionString = secret.Value;
+}
 
 //ITitleRepository titleRepository = TitleRepositoryList.Instance;
 ITitleRepository titleRepository = new TitleRepositorySql(connectionString);
